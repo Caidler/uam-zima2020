17adf400ac02c7b96aa7512d2f79d5190466b245
Caidler <rafal.szalkiewicz@gmail.com>
2020-01-04 01:21:02 +0100

    hotfix

--- battle-engine/src/main/java/pl.psi.battleengine/spellbook/SpellIf.java
+++ battle-engine/src/main/java/pl.psi.battleengine/spellbook/SpellIf.java
@@ -3,7 +3,6 @@ package pl.psi.battleengine.spellbook;
 import java.awt.*;
 
 public interface SpellIf {
-//pizda ogień
     public int howManyTurns();
     public String name();
     public Point point();
